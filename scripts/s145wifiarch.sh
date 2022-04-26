116633ccc8016b1ee9b4f1ae375ed56e452d99dc
alexan2dre123 <xandi10beck@gmail.com>
2022-04-26 11:20:04 -0300

    update

--- scripts/s145wifiarch.sh
+++ scripts/s145wifiarch.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-cd /lib/firmware/ath10k/QCA9377/hw1.0/ || exit
-sudo mv firmware-5.bin.xz firmware-5.bin.orig
-sudo mv firmware-6.bin.xz firmware-6.bin.orig
-sudo wget "https://github.com/kvalo/ath10k-firmware/blob/master/QCA9377/hw1.0/CNSS.TF.1.0/firmware-5.bin_CNSS.TF.1.0-00267-QCATFSWPZ-1?raw=true" -O firmware-5.bin
-echo 'Restarting WiFi Driver...'; sleep 5
-sudo modprobe -r ath10k_pci
-sudo modprobe ath10k_pci
-echo 'Done'; sleep 3
+if which wget ; then    
+    cd /lib/firmware/ath10k/QCA9377/hw1.0/ || exit
+    sudo mv firmware-5.bin.xz firmware-5.bin.orig
+    sudo mv firmware-6.bin.xz firmware-6.bin.orig
+    sudo wget "https://github.com/kvalo/ath10k-firmware/blob/master/QCA9377/hw1.0/CNSS.TF.1.0/firmware-5.bin_CNSS.TF.1.0-00267-QCATFSWPZ-1?raw=true" -O firmware-5.bin
+    echo 'Restarting WiFi Driver...'; sleep 5
+    sudo modprobe -r ath10k_pci
+    sudo modprobe ath10k_pci
+    echo 'Done'; sleep 3
+else
+    sudo pacman -S wget
+fi
