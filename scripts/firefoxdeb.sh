7ff7013cbccdb05b11788fb7a439fdb8c5fd550f
alexan2dre123 <alexandrebeck@protonmail.com>
2022-08-08 13:27:55 -0300

    fixes + new install scripts

---
+++ scripts/firefoxdeb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo 'This will remove the snap version of firefox and install the .deb version using the official mozilla ppa...'; sleep 5
+sudo snap remove firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+sudo apt install firefox
+
--- scripts/installhelper.sh
+++ scripts/installhelper.sh
@@ -112,14 +112,17 @@ do
                         0) exit;;
                     esac
                 else
-                    echo 'Installing Chaotic AUR...'; sleep 2
-                    sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-                    sudo pacman-key --lsign-key FBA220DFC880C036
-                    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-                    echo '[chaotic-aur]
-                    Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a  /etc/pacman.conf
+                    echo 'Installing Chaotic AUR...' ; sleep 2
+		            sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+	                sudo pacman-key --lsign-key FBA220DFC880C036
+		            sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+                    echo '
+                    [chaotic-aur]
+                    Include = /etc/pacman.d/chaotic-mirrorlist
+                    ' | sudo tee -a  /etc/pacman.conf
+                    echo 'Updating mirrors...' ; sleep 2
                     sudo pacman -Syyuu
-                    echo "Done"; sleep 2
+                    echo 'Done' ; sleep 3
                 fi
             fi
         ;;
