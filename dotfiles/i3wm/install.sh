05c57a653f8c83dd3443d86709753ba4ead1a164
alexan2dre123 <alexandrebeck@protonmail.com>
2023-05-22 16:57:10 -0300

    install script fixes

--- dotfiles/i3wm/install.sh
+++ dotfiles/i3wm/install.sh
@@ -6,10 +6,10 @@ configPath="/home/$USER/.config/"
 currentPath=$(pwd)
 
 printf "Installing Packages"; sleep 2
-sudo pacman -Syu rofi lxappearance maim xclip feh thunar thunar-archive-plugin xfce4-terminal papirus-icon-theme ttf-font-awesome ttf-fira-sans ttf-fira-code noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji xarchiver unzip zip p7zip pigz gzip unrar mate-polkit git wget curl
+sudo pacman -Syu rofi lxappearance maim xclip feh thunar thunar-archive-plugin xfce4-terminal papirus-icon-theme ttf-font-awesome ttf-fira-sans ttf-fira-code noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji xarchiver unzip zip p7zip pigz gzip unrar mate-polkit git wget curl gvfs xdg-user-dirs
 git clone https://github.com/EliverLara/Kripton.git
-sudo mv "$currentPath/Kripton" usr/share/themes/
-
+sudo mv "$currentPath/Kripton" /usr/share/themes/
+xdg-user-dirs-update
 printf "Setting up..."; sleep 3
 cp -r i3 "$configPath"
 cp -r i3blocks "$configPath"
