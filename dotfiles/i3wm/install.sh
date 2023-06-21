3ff2f54751515264ad5ac9567b6d8e56e6530134
alexan2dre123 <alexandrebeck@protonmail.com>
2023-06-21 18:16:11 -0300

    i3wm config update

--- dotfiles/i3wm/install.sh
+++ dotfiles/i3wm/install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 clear
-set -e
-
 configPath="/home/$USER/.config/"
 currentPath=$(pwd)
-
-printf "Installing Packages"; sleep 2
-sudo pacman -Syu rofi lxappearance maim xclip feh thunar thunar-archive-plugin xfce4-terminal papirus-icon-theme ttf-font-awesome ttf-fira-sans ttf-fira-code noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji xarchiver unzip zip p7zip pigz gzip unrar mate-polkit git wget curl gvfs xdg-user-dirs
+printf "Installing Packages..."; sleep 2
+if which apt > /dev/null 2>&1; then
+    sudo apt install i3 i3blocks rofi lxappearance maim xclip feh thunar thunar-archive-plugin xfce4-terminal papirus-icon-theme xarchiver unzip zip p7zip pigz gzip unrar mate-polkit git wget curl gvfs xdg-user-dirs fonts-noto fonts-font-awesome fonts-firacode 
+elif which pacman > /dev/null 2>&1; then
+    sudo pacman -Syu i3 rofi lxappearance maim xclip feh thunar thunar-archive-plugin xfce4-terminal papirus-icon-theme ttf-font-awesome ttf-fira-sans ttf-fira-code noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji xarchiver unzip zip p7zip pigz gzip unrar mate-polkit git wget curl gvfs xdg-user-dirs
+fi
 git clone https://github.com/EliverLara/Kripton.git
 sudo mv "$currentPath/Kripton" /usr/share/themes/
 xdg-user-dirs-update
+clear
 printf "Setting up..."; sleep 3
 cp -r i3 "$configPath"
 cp -r i3blocks "$configPath"
 cp -r rofi "$configPath"
 cp -r wallpaper.jpg "/home/$USER/Pictures/"
+cp -r .gtkrc-2.0.mine "/home/$USER/"
+printf " Done!"; sleep 3
 
 
