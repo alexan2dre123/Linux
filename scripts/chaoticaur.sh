c13781796cdc631fcd96638459764f3677403cef
Alexandre Beck <34386707+alexan2dre123@users.noreply.github.com>
2023-07-10 19:50:14 -0300

    Update chaoticaur.sh

--- scripts/chaoticaur.sh
+++ scripts/chaoticaur.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Installing Chaotic AUR...' ; sleep 2
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo '[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a  /etc/pacman.conf
